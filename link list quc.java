2e1aae0afa6a3c14e4d11edc0b7c3a5f458f9de9
Lahiru Gayashan <gayashan732@gmail.com>
2022-06-20 23:41:42 +0530

    Create link list quc.java

---
+++ link list quc.java
@@ -0,0 +1,32 @@
+// linked list is used to store some employee data namely employee number (integer) and name (string).
+
+//i) Implement Link class for the above linked list with suitable attributes and methods.
+class Link{
+    public int number;
+    public int name;
+    public next;
+
+ public Link(int no,string name){
+    number=num;
+    name=name;
+    next=null;
+ }   
+ public void display(){
+    System.out.println(number);
+    System.out.println(name);
+ }
+}
+
+//ii) Implement Linked List class with suitable attributes and the constructor.
+class LinkList(){
+    Private link first;
+    public Link List(){
+        first=null;
+    }
+}
+
+
+//iii) Write sortedln~ert (int empNo, String name)method in Linked List class to insert the data into the linked list in ascending 0 'der. 
+
+
+//iv) Write displayList ( ) method in Linked List clas to display all the links in the linked list.
